482c2e300c75a394872500364391e74e5bc740be
cnanduri <nlkchaitanya@gmail.com>
2017-03-11 11:56:30 -0500

    Installed LeakCanary to detect memory leaks in the application

---
+++ app/src/main/java/app/udacity/android/cn/popularmovies/PopularMoviesApplication.java
@@ -0,0 +1,27 @@
+package app.udacity.android.cn.popularmovies;
+
+import android.app.Application;
+
+import com.squareup.leakcanary.LeakCanary;
+
+/**
+ * Created by Chaitanya on 3/11/2017.
+ * Application class defined for Popular Movies App.
+ * <p>
+ * Added code for using LeakCanary to detect memory leaks in the app.
+ */
+
+public class PopularMoviesApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
+        // Normal app init code...
+    }
+}
